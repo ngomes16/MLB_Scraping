4d9b00a019d4b4716c9d67936068ee413d4543b1
ngomes16 <gomes3@illinois.edu>
2024-07-11 18:49:11 -0500

    Remove cities from formatted data

--- data_creation/yahoo_scraper.py
+++ data_creation/yahoo_scraper.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-    
 url = "https://sports.yahoo.com/mlb/odds/"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
@@ -25,7 +23,7 @@ for game in games:
         team_spans = item.find_all("span", class_="Fw(600) Pend(4px) Ell D(ib) Maw(190px) Va(m)")
         
         for team_span in team_spans:
-            team_names.append(team_span.text)
+            team_names.append(team_span.text.strip())
         
         # Find all spans with the class "Lh(19px)" and get the third instance
         associated_spans = item.find_all("span", class_="Lh(19px)")
@@ -37,7 +35,9 @@ for game in games:
 
     # Ensure there are exactly two team names and an associated number
     if len(team_names) == 2 and associated_number is not None:
-        odds_data[tuple(team_names)] = associated_number
+        # Sort team names alphabetically
+        sorted_teams = tuple(sorted(team_names))
+        odds_data[sorted_teams] = associated_number
 
 # Print the dictionary
 print(odds_data)
